650a99fd97728aa4fd1206dfe1cb5bd984ad196f
Arpitha Alur <arpithaalur7@gmail.com>
2019-07-14 23:51:45 -0700

    Commit 3

--- js/script.js
+++ js/script.js
@@ -5,4 +5,29 @@ $(document).ready(function(){
     play : 5000,
     pagination : false
   });
+
+  var typed = new Typed(".typed",{
+
+      strings: ["Software Engineer.", "Data Anlytics Enthusiast."],
+      typeSpeed: 70,
+      loop: true,
+      startDelay: 1000,
+      showCursor: false
+  });
+  $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
 });

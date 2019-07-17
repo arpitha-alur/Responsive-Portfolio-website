fd528ee5679e470613d03518064a8f70f1bb6a89
Arpitha Alur <arpithaalur7@gmail.com>
2019-07-16 23:45:59 -0700

    commit3

--- js/script.js
+++ js/script.js
@@ -14,20 +14,44 @@ $(document).ready(function(){
       startDelay: 1000,
       showCursor: false
   });
+
   $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:10,
-        nav:true,
+        items:4,
         responsive:{
             0:{
                 items:1
             },
-            600:{
+            480:{
+                items:2
+            },
+            768:{
                 items:3
             },
-            1000:{
-                items:5
+            938:{
+                items:4
             }
+
+        }
+    });
+
+    //jquery object, to activate chart on scroll when we reach skills section
+    var skillsTopOffset = $(".skillssection").offset().top;
+    console.log(skillsTopOffset);
+    $(window).scroll(function(){
+        if(window.pageYOffset > skillsTopOffset - $(window).height() +200){
+            
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from,to,percent){
+                    $(this.el).find('.percent').text(Math.round(percent))
+                }
+            });
         }
     });
 });

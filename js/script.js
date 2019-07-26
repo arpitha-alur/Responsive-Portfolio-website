1c5e4c026f3cf37c98a25cc5969ee6358728d679
Arpitha Alur <arpithaalur7@gmail.com>
2019-07-26 01:08:03 -0700

    pre-final

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
   var typed = new Typed(".typed",{
 
-      strings: ["Software Engineer.", "Data Anlytics Enthusiast."],
+      strings: ["Developer", "Data Anlytics Enthusiast"],
       typeSpeed: 70,
       loop: true,
       startDelay: 1000,
@@ -58,11 +58,39 @@ $(document).ready(function(){
     $("[data-fancybox]").fancybox();
 
     $(".items").isotope({
+        layoutMode:'fitRows',
         filter: '*',
         animationOptions:{
             duration:1500,
             easing:'linear',
             queue: false
         }
-    })
+    });
+
+     
+    $("#navigation li a").click(function(e){
+         e.preventDefault();
+
+         var targetElement = $(this).attr("href");
+         var targetPosition = $(targetElement).offset().top;
+         $("html,body").animate({scrollTop:targetPosition - 50}, "slow");
+    });
+
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll",stickyNavigation);
+
+    function stickyNavigation(){
+        var body = $("body");
+        if($(window).scrollTop() >= navTop){
+            body.css("padding-top",nav.outerHeight() + "px");
+            body.addClass("fixedNav");
+        }
+
+        else{
+            body.css("padding-top",0);
+            body.removeClass("fixedNav");
+        }
+    }
 });
